bbc4e79389d9a17dcf2c41b4c6231ddcf2dee6b2
Bill Crooks <51101371+bcrooks-04@users.noreply.github.com>
2020-08-30 16:45:12 +1000

    Map viewer draws the exported texture

--- RaylibGame/Scenes/MapViewer.cs
+++ RaylibGame/Scenes/MapViewer.cs
@@ -1,8 +1,12 @@
-﻿using RaylibGame.Engine;
+﻿using Raylib_cs;
+using RaylibGame.Engine;
 
 namespace RaylibGame.Scenes {
     public class MapViewer : IScene {
+        private Texture2D _mapTexture;
+        
         public ReturnActions Start() {
+            _mapTexture = Raylib.LoadTexture("export.png");
             return ReturnActions.ReturnNull;
         }
 
@@ -11,10 +15,12 @@ namespace RaylibGame.Scenes {
         }
 
         public ReturnActions Render() {
+            Raylib.DrawTexture(_mapTexture, 0, 0, Color.WHITE);
             return ReturnActions.ReturnNull;
         }
 
         public ReturnActions Close() {
+            Raylib.UnloadTexture(_mapTexture);
             return ReturnActions.ReturnNull;
         }
     }
