47d6d0c3606ff9f28106732818be6da28b9fa15d
Bill Crooks <51101371+bcrooks-04@users.noreply.github.com>
2020-09-07 13:58:20 +1000

    Added region viewer scene

---
+++ RaylibGame/Scenes/RegionViewer.cs
@@ -0,0 +1,21 @@
+﻿using RaylibGame.Engine;
+
+namespace RaylibGame.Scenes {
+    public class RegionViewer : IScene {
+        public ReturnActions Start() {
+            return ReturnActions.ReturnNull;
+        }
+
+        public ReturnActions Update() {
+            return ReturnActions.ReturnNull;
+        }
+
+        public ReturnActions Render() {
+            return ReturnActions.ReturnNull;
+        }
+
+        public ReturnActions Close() {
+            return ReturnActions.ReturnNull;
+        }
+    }
+}
